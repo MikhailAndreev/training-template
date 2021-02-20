87ee74eae824de2b085e2cddb5d74d3f965fae65
Soyunov Hatam <ksoyunov@i-neti.ru>
2021-02-20 12:57:31 +0300

    refactoring

--- src/modules/news/NewsService.ts
+++ src/modules/news/NewsService.ts
@@ -1,23 +1,26 @@
 import NewsApiRepository from './repositories/NewsApiRepository';
 import { NewsFactory } from './NewsFactory';
 import { News } from './models/News';
+import NewsLocalRepository from './repositories/NewsLocalRepository';
 
 export default class NewsService {
-  newsAPI: NewsApiRepository;
+  newsApi: NewsApiRepository;
+  newsLocal: NewsLocalRepository;
   newsFactory: NewsFactory;
 
   constructor() {
-    this.newsAPI = new NewsApiRepository();
+    this.newsApi = new NewsApiRepository();
+    this.newsLocal = new NewsLocalRepository();
     this.newsFactory = new NewsFactory();
   }
 
   getAll = async (): Promise<News[]> => {
-    const { data } = await this.newsAPI.getAll();
+    const { data } = await this.newsApi.getAll();
     return this.newsFactory.createList<News>(News, data).slice(0, 12); // cut list for demo
   };
 
   getOne = async (id: number): Promise<News> => {
-    const { data } = await this.newsAPI.getOne(id);
+    const { data } = await this.newsApi.getOne(id);
     return this.newsFactory.create<News>(News, data);
   };
 }
