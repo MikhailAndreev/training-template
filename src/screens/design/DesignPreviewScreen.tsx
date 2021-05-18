4e4f736498e5dff46b7cc0836fa3fccbd976d83c
MikhailAndreev <g_ineti_andreevme@cds.spb>
2021-05-18 08:55:36 +0300

    configure components

--- src/screens/design/DesignPreviewScreen.tsx
+++ src/screens/design/DesignPreviewScreen.tsx
@@ -1,6 +1,42 @@
-import { Box, Container, Typography } from '@material-ui/core';
+import { useState } from 'react';
+import {
+  Box,
+  Button,
+  Container,
+  makeStyles,
+  Paper,
+  Typography,
+  Theme,
+  FormLabel,
+  TextField,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Tabs,
+  Tab,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from '@material-ui/core';
+import { Autocomplete, Pagination, ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const DesignPreviewScreen: React.FC = () => {
+  const classes = useStyles();
+
+  const [tabValue, setTabValue] = useState('test1');
+  const [toggleBtnValue, setToggleBtnValue] = useState('test1');
+
+  const handleChangeTab = (event: any, newValue: any) => {
+    setTabValue(newValue);
+  };
+  const handleChangeToggleBtn = (event: any, newValue: any) => {
+    setToggleBtnValue(newValue);
+  };
+
   const typographyVariants = [
     { variant: 'h1' },
     { variant: 'h2' },
@@ -12,6 +48,20 @@ const DesignPreviewScreen: React.FC = () => {
     { variant: 'button' },
     { variant: 'caption' },
   ];
+
+  const top100Films = [
+    { title: 'The Shawshank Redemption', year: 1994 },
+    { title: 'The Godfather', year: 1972 },
+    { title: 'The Godfather: Part II', year: 1974 },
+    { title: 'The Dark Knight', year: 2008 },
+    { title: '12 Angry Men', year: 1957 },
+  ];
+
+  const createData = (orderName: any, name: any, phone: any, complex: any, corpus: any) => {
+    return { orderName, name, phone, complex, corpus };
+  };
+
+  const rows = [createData('5312432', 'Градостроительный Д. В.', '+7 (904) 234-32-99', 'ЦДС «Елизаровский»', '3')];
 
   const renderTypography = () => {
     return (
@@ -42,8 +92,179 @@ const DesignPreviewScreen: React.FC = () => {
           <Typography variant="h2">Typography</Typography>
         </Box>
 
-        <Box display="flex" flexDirection="column">
+        <Box display="flex" flexDirection="column" mb={8}>
           {renderTypography()}
+        </Box>
+
+        <Box display="flex" flexDirection="column" mb={6}>
+          <Box mb={3}>
+            <Typography variant="h2">Buttons</Typography>
+          </Box>
+
+          <Box display="flex">
+            <Box mr={4}>
+              <Button variant="contained" color="primary">
+                Contained
+              </Button>
+            </Box>
+
+            <Box mr={4}>
+              <Button variant="outlined" color="primary">
+                Outlined
+              </Button>
+            </Box>
+
+            <Box mr={4}>
+              <Button variant="text">Text</Button>
+            </Box>
+
+            <Box>
+              <Button variant="text" color="primary">
+                Text primary
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+
+        <Box display="flex" flexDirection="column" mb={6}>
+          <Box mb={3}>
+            <Typography variant="h2">Paper</Typography>
+          </Box>
+
+          <Paper className={classes.paper} variant="outlined">
+            Paper
+          </Paper>
+        </Box>
+
+        <Box display="flex" flexDirection="column" mb={6}>
+          <Box mb={3}>
+            <Typography variant="h2">Form</Typography>
+          </Box>
+
+          <Box>
+            <FormLabel>Form label</FormLabel>
+
+            <TextField fullWidth variant="outlined" size="small" name="test" placeholder="Placeholder" />
+          </Box>
+        </Box>
+
+        <Box display="flex" flexDirection="column" mb={6}>
+          <Box mb={3}>
+            <Typography variant="h2">Autocomplete</Typography>
+          </Box>
+
+          <Box>
+            <Autocomplete
+              multiple
+              id="tags-outlined"
+              options={top100Films}
+              getOptionLabel={(option: any) => option.title}
+              defaultValue={[top100Films[2]]}
+              filterSelectedOptions
+              renderInput={params => (
+                <TextField {...params} variant="outlined" label="Test autocomplete" placeholder="Favorites" />
+              )}
+            />
+          </Box>
+        </Box>
+
+        <Box display="flex" flexDirection="column" mb={6}>
+          <Box mb={3}>
+            <Typography variant="h2">Toggle buttons</Typography>
+          </Box>
+
+          <Box>
+            <ToggleButtonGroup onChange={handleChangeToggleBtn} exclusive value={toggleBtnValue}>
+              <ToggleButton value="test1">Test 1</ToggleButton>
+              <ToggleButton value="test2">Test 2</ToggleButton>
+            </ToggleButtonGroup>
+          </Box>
+        </Box>
+
+        <Box display="flex" flexDirection="column" mb={6}>
+          <Box mb={3}>
+            <Typography variant="h2">Tabs</Typography>
+          </Box>
+
+          <Box>
+            <Paper square>
+              <Tabs
+                onChange={handleChangeTab}
+                value={tabValue}
+                indicatorColor="primary"
+                textColor="primary"
+                aria-label="disabled tabs example"
+              >
+                <Tab value="test1" label="Test tab 1" />
+                <Tab value="test2" label="Test tab 2" />
+                <Tab value="test3" label="Test tab 3" />
+              </Tabs>
+            </Paper>
+          </Box>
+        </Box>
+
+        <Box display="flex" flexDirection="column" mb={6}>
+          <Box mb={3}>
+            <Typography variant="h2">Accordion</Typography>
+          </Box>
+
+          <Box>
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+                <Typography>Accordion 1</Typography>
+              </AccordionSummary>
+
+              <AccordionDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet
+                  blandit leo lobortis eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </Box>
+        </Box>
+
+        <Box display="flex" flexDirection="column" mb={6}>
+          <Box mb={3}>
+            <Typography variant="h2">Table</Typography>
+          </Box>
+
+          <Box>
+            <TableContainer>
+              <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="left">№ брони</TableCell>
+                    <TableCell align="left">ФИО</TableCell>
+                    <TableCell align="left">телефон</TableCell>
+                    <TableCell align="left">жк</TableCell>
+                    <TableCell align="left">корпус</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row: any) => (
+                    <TableRow key={row.name}>
+                      <TableCell align="left">{row.orderName}</TableCell>
+                      <TableCell align="left">{row.name}</TableCell>
+                      <TableCell align="left">{row.phone}</TableCell>
+                      <TableCell align="left">{row.complex}</TableCell>
+                      <TableCell align="left">{row.corpus}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Box>
+        </Box>
+
+        <Box display="flex" flexDirection="column" mb={6}>
+          <Box mb={3}>
+            <Typography variant="h2">Pagination</Typography>
+          </Box>
+
+          <Box>
+            <Pagination count={10} variant="outlined" shape="rounded" />
+          </Box>
         </Box>
       </Box>
     </Container>
@@ -51,3 +272,22 @@ const DesignPreviewScreen: React.FC = () => {
 };
 
 export default DesignPreviewScreen;
+
+const useStyles = makeStyles((theme: Theme) => ({
+  paper: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 150,
+    height: 300,
+    padding: 20,
+    cursor: 'pointer',
+    transition: 'all .4s ease',
+    '&:hover': {
+      transform: 'translateY(-2px)',
+    },
+  },
+  table: {
+    minWidth: 650,
+  },
+}));
--- src/styles/MuiTheme.ts
+++ src/styles/MuiTheme.ts
@@ -1,4 +1,27 @@
 import { createMuiTheme } from '@material-ui/core';
+import '@material-ui/core/styles/createPalette';
+
+interface surface {
+  HighEmphasis: string;
+  MediumEmphasis: string;
+  LowEmphasis: string;
+  Background: string;
+  Stroke: string;
+  Filled: string;
+  Disabled: string;
+  Light: string;
+}
+
+declare module '@material-ui/core/styles/createPalette' {
+  interface Palette {
+    surface: Partial<surface>;
+    prim: Palette['grey'];
+  }
+  interface PaletteOptions {
+    surface: Partial<surface>;
+    prim: PaletteOptions['grey'];
+  }
+}
 
 export const theme = createMuiTheme({
   breakpoints: {
@@ -15,6 +38,7 @@ export const theme = createMuiTheme({
   palette: {
     primary: { main: '#18A6F5' },
     secondary: { main: '#939CAB' }, // посмотрел в ЦДС
+    //Object literal may only specify known properties, and 'prim' does not exist in type 'PaletteOptions'
     prim: {
       50: '#F6FAFC',
       100: '', // ?????
@@ -36,7 +60,7 @@ export const theme = createMuiTheme({
       Filled: '#B4C8D1',
       Disabled: '#C2C6CB',
     },
-    error: { main: '#ED2D23' },
+    error: { main: '#ED2D23' }, // взял из ui-kit
   },
   typography: {
     fontFamily: "'Ubuntu', sans-serif",
@@ -116,26 +140,227 @@ export const theme = createMuiTheme({
       },
     },
     MuiButton: {
+      root: {
+        height: 48,
+        padding: '0 12px',
+        borderRadius: '4px',
+      },
+      contained: {}, // вопрос почему на цдс белый bg и синий цвет ????
+      containedPrimary: {
+        color: '#fff',
+        backgroundColor: '#18A6F5',
+        boxShadow: 'none',
+        '&:hover': {},
+      },
+      outlined: {},
+      outlinedPrimary: {
+        border: '2px solid #18A6F5',
+        backgroundColor: 'transparent',
+        '&:hover': {
+          border: '2px solid #18A6F5',
+        },
+      },
       text: {
         backgroundColor: 'transparent',
         transition: 'all .4s ease',
         '&:hover': {
-          color: '',
+          color: '#18A6F5',
           backgroundColor: 'transparent',
         },
       },
       textPrimary: {
-        color: '',
+        color: '#18A6F5',
         '&:hover': {
           color: '',
           backgroundColor: 'transparent',
         },
       },
       textSecondary: {
-        color: '',
+        color: '#939CAB',
         '&:hover': {
           color: '',
           backgroundColor: 'transparent',
+        },
+      },
+    },
+
+    MuiPaper: {
+      outlined: {
+        border: '1px solid #C9DEE8',
+      },
+    },
+    MuiFormLabel: {
+      root: {
+        display: 'block',
+        fontSize: 11,
+        lineHeight: '16px',
+        fontWeight: 500,
+        letterSpacing: '0.07em',
+        textTransform: 'uppercase',
+        marginBottom: 8,
+      },
+    },
+
+    MuiInputBase: {
+      input: {
+        padding: '16px',
+      },
+    },
+
+    MuiOutlinedInput: {
+      root: {
+        height: '56px',
+        background: '#F6FAFC',
+        '&:hover .MuiOutlinedInput-notchedOutline': {
+          borderColor: '#18A6F5',
+        },
+      },
+
+      input: {
+        padding: '16px',
+      },
+      notchedOutline: {
+        borderColor: '#C9DEE8',
+      },
+    },
+    // @ts-ignore
+    MuiToggleButtonGroup: {
+      root: {
+        backgroundColor: '#F6FAFC',
+      },
+    },
+    MuiToggleButton: {
+      root: {
+        fontSize: '13px',
+        lineHeight: '16px',
+        color: '#001333',
+        borderRadius: 4,
+        fontWeight: 400,
+        textTransform: 'none',
+        borderColor: '#C9DEE8',
+        '&.Mui-disabled': {
+          color: 'rgba(0, 0, 0, .4)',
+        },
+        '&.Mui-selected': {
+          color: '#FFFFFF',
+          borderColor: '#18A6F5',
+          backgroundColor: '#18A6F5',
+          zIndex: 1,
+          '&.Mui-selected:hover': {
+            backgroundColor: '#18A6F5',
+          },
+        },
+      },
+    },
+
+    MuiAccordion: {
+      root: {
+        border: '1px solid #C9DEE8',
+        boxShadow: 'none',
+      },
+    },
+    MuiAccordionDetails: {
+      root: {
+        padding: '10px 62px',
+      },
+    },
+    MuiAccordionSummary: {
+      root: {
+        height: 66,
+        padding: '0 62px',
+      },
+      content: {
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      },
+    },
+
+    MuiTableHead: {
+      root: {
+        textTransform: 'uppercase',
+        backgroundColor: '#F6FAFC',
+      },
+    },
+    MuiTableCell: {
+      root: {
+        borderBottom: '1px solid #C9DEE8',
+      },
+      head: {
+        color: '#3B4961',
+        fontWeight: 500,
+        fontSize: 11,
+        lineHeight: '16px',
+        padding: '14px 12px',
+        whiteSpace: 'nowrap',
+      },
+      body: {
+        fontWeight: 400,
+        padding: 12,
+        borderBottom: 'none',
+      },
+    },
+
+    MuiPaginationItem: {
+      root: {
+        fontSize: 13,
+        fontWeight: 500,
+        lineHeight: '16px',
+        width: 48,
+        height: 48,
+        margin: '0 8px',
+      },
+      page: {
+        '&.Mui-selected': {
+          color: '#fff',
+          borderColor: '#18A6F5',
+          backgroundColor: '#18A6F5',
+          '&:hover': {
+            borderColor: '#3BB3F7 !important',
+            backgroundColor: '#3BB3F7 !important',
+          },
+        },
+      },
+      ellipsis: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 48,
+        height: 48,
+        border: '1px solid #C9DEE8',
+        borderRadius: 4,
+      },
+      outlined: {
+        border: '1px solid #C9DEE8',
+      },
+    },
+
+    MuiAutocomplete: {
+      root: {
+        '& .MuiAutocomplete-tag': {
+          height: 40,
+          margin: '3px 6px',
+          borderRadius: 4,
+          border: '1px solid #C9DEE8',
+          backgroundColor: '#F6FAFC',
+          '& .MuiChip-label': {
+            fontWeight: 400,
+            fontSize: 13,
+            lineHeight: '16px',
+            letterSpacing: '.03333em',
+          },
+        },
+        '& .MuiOutlinedInput-root': {
+          height: 56,
+          padding: '5px 18px !important',
+        },
+      },
+      endAdornment: {
+        right: '18px !important',
+        '& .MuiIconButton-root': {
+          color: '#B4C8D1',
+          '&:hover': {
+            color: '#939CAB',
+          },
         },
       },
     },
